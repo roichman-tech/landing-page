fe52906dc73e76407c781f041aa7b116efac8640
Matheus Roichman <roichmankill@gmail.com>
2026-01-02 00:01:27 -0300

    feat: build.inlineStylesheets to always

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,17 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-  alias: {
-    '@': './src',
-  },
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	alias: {
+		"@": "./src",
+	},
+	build: {
+		// Inline built CSS to avoid a separate render-blocking stylesheet request.
+		inlineStylesheets: "always",
+	},
 });
