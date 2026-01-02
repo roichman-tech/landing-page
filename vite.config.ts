7cf652407f44c52b86b767be22b4f33590084507
Matheus Roichman <roichmankill@gmail.com>
2026-01-01 22:26:39 -0300

    refactor: removed port 8080 from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-	server: {
-		host: "::",
-		port: 8080,
-	},
 	plugins: [react()],
 	resolve: {
 		alias: {
