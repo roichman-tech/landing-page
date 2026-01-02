d728d88f587b885a33a87a292912e1c57f8b7e60
Matheus Roichman <roichmankill@gmail.com>
2026-01-01 23:00:35 -0300

    perf: moved CSS import to HTML

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App.tsx";
 
-import "./index.css";
-
 const rootElement = document.getElementById("root");
 
 if (!rootElement) {
