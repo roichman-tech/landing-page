b95c7ef40697d58269f0b34460b32b3176c4cd72
Matheus Roichman <roichmankill@gmail.com>
2026-01-02 00:23:12 -0300

    refactor: used Astro layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	integrations: [
-		react(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-	],
-	alias: {
-		"@": "./src",
-	},
-	build: {
-		// Inline built CSS to avoid a separate render-blocking stylesheet request.
-		inlineStylesheets: "always",
-	},
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  alias: {
+    '@': './src',
+  },
+  build: {
+    // Inline built CSS to avoid a separate render-blocking stylesheet request.
+    inlineStylesheets: 'always',
+  },
 });
