3107af3ff335e7177270b8692b8a17658ce8bcf6
Matheus Roichman <roichmankill@gmail.com>
2026-01-01 22:55:46 -0300

    naming issue

--- src/view/pages/home/index.tsx
+++ src/view/pages/home/index.tsx
@@ -1,8 +1,8 @@
 import { Layout } from "@/view/layouts/layout";
-import { CTA } from "@/view/pages/home/components/CTA";
-import { Hero } from "@/view/pages/home/components/Hero";
-import { Process } from "@/view/pages/home/components/Process";
-import { Solutions } from "@/view/pages/home/components/Solutions";
+import { CTA } from "@/view/pages/home/components/cta-";
+import { Hero } from "@/view/pages/home/components/hero-";
+import { Process } from "@/view/pages/home/components/process-";
+import { Solutions } from "@/view/pages/home/components/solutions-";
 
 export function HomePage() {
 	return (
